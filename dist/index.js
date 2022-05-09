5b404cbf05813a238c7ed9552442e4db04892835
archaic10 <vitor.franca.9400@gmail.com>
2022-05-09 16:35:21 -0300

    test: install dependency by directory

--- dist/index.js
+++ dist/index.js
@@ -13471,19 +13471,18 @@ async function getContentFile (raw_url){
 async function modifyVersionAndUploadFile(data, sha, newVersion){
     if (data && data != ''){
         try{
-            // if(path.split('/').length > 1){
-            //     console.log('with dir')
-            //     let dir = path.replace("package.json", "")
-            //     await exec(`ls`)
-            //     await exec(`cd ${dir}`)
-            //     await exec("yarn install -W")
-            // }else{
-            //     console.log('without dir')
-            //     await exec("yarn install")
-            // }
-            
-            console.log('show path: ',path)
-            let fileRead = fs.readFileSync(`.${path}`, 'utf8').toString()
+            if(path.split('/').length > 1){
+                console.log('with dir')
+                let dir = path.replace("package.json", "")
+                await exec(`ls`)
+                await exec(`cd ${dir}`)
+            }else{
+                console.log('without dir')
+            }
+            console.log('after ...')
+            await exec(`ls`)
+            console.log('show path', path)
+            let fileRead = fs.readFileSync(`./package.json`, 'utf8').toString()
             let defaultVersion = /"version":[\s]+"([v0-9|0-9]+).([0-9]+).([0-9]+)"/
             newVersion = newVersion.split(/([a-z]|[A-z])+\.*/).pop()
             fileRead = fileRead.replace(defaultVersion, `"version": "${newVersion}"`)
